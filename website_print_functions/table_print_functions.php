1dd03a4e4dbe7ad943ca9cc6d23b8bf11b5bb6f6
richardbendler <richard.bendler@gmail.com>
2022-04-12 19:48:53 +0200

    trying responsitivity

--- website_print_functions/table_print_functions.php
+++ website_print_functions/table_print_functions.php
@@ -505,7 +505,7 @@
                 <div style='position:absolute; text-align:center'>"; if($platzierungen[0] != "platzhalter"){$x=$platzierungen[0];echo "<h3>$x</h3>";}else{echo "<h3><i>noch nicht bestimmt</i></h3>";} echo"</div>
             </div>
             <div style='display:grid; grid-template-columns: 1fr 1fr 1fr'>
-                <div style='position:absolute; text-align:right'>"; if($platzierungen[1] != "platzhalter"){$x=$platzierungen[1];echo "<h3>$x</h3>";}else{echo "<h3><i>noch nicht bestimmt</i></h3>";} echo"</div>
+                <div style='position:absolute; right: 20px'>"; if($platzierungen[1] != "platzhalter"){$x=$platzierungen[1];echo "<h3>$x</h3>";}else{echo "<h3><i>noch nicht bestimmt</i></h3>";} echo"</div>
                 <div></div>
                 <div style='background-color:red'><h1>1. &#10026;</h1></div>
                 <div></div>
