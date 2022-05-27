83ebe464b4f1c75b772c0c304ab58ead85a9ffe5
richardbendler <richard.bendler@gmail.com>
2022-05-27 20:26:06 +0200

    mail

--- website_datachange/edit_teams.php
+++ website_datachange/edit_teams.php
@@ -74,13 +74,13 @@ include_once 'edit_interface.php';
 		mail_att("kummerkasten@REDACTED.de", $fromEmail, "Neues Team angemeldet: ".$name, $message);
 
 		//an Team
-		$fromEmail = "kummerkasten@REDACTED.de";
+		/*$fromEmail = "kummerkasten@REDACTED.de";
 		$team_mail = $_POST['Mail']
 		$name = $_POST['Teamname'];
 		$message = "";
 		$message .= $infoVomAngemeldetenTeam;
 		$message .=  "\r\n" . "Bei Fragen oder Wünschen, schreib uns gern einfach eine Mail!";
-		mail_att($team_mail, $fromEmail, "Team erfolgreich angemeldet: ".$name, $message);
+		mail_att($team_mail, $fromEmail, "Team erfolgreich angemeldet: ".$name, $message);*/
 
 		//WEITERLEITUNG ZURÜCK - mit eventueller TestTurnierID
 		$test_turnier_id = $_GET['test_turnier_id'];
