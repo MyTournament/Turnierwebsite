1b97a04a84f7099aff613d24e68a1e30c65b55de
richardbendler <richard.bendler@gmail.com>
2022-05-27 19:34:37 +0200

    history

--- website_functionalities/test_turnier_mode.php
+++ website_functionalities/test_turnier_mode.php
@@ -4,7 +4,36 @@
     if($test_turnier_id == NULL){
         $test_turnier_id = $_POST["test_turnier_id"];
     }
-    if ($test_turnier_id != 0) {
+    $history_turnier_id = 0;
+    $history_turnier_id = $_GET["history_turnier_id"];
+    if($history_turnier_id == NULL){
+        $history_turnier_id = $_POST["history_turnier_id"];
+    }
+    if ($history_turnier_id != 0) {
+        //TurnierID überschreiben weil ich in den Test-Modus möchte
+        $TurnierID = $history[$history_turnier_id][1]; //kommt aus variables.php
+        $TurnierName = $history[$history_turnier_id][2];
+        echo "
+        <table class='th-text-center'> <!-- class='withBorderCollapse'  -->
+            <thead style='background-color:#7700FF;'>
+                <tr>
+                    <td style='color:white; text-align: center;'>Du befindest dich in der History-Ansicht! ($TurnierName)</td> <!-- #7700FF -->
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style='text-align: center;'>
+                        <form style='color:#00FF00;margin: 0 0 0 0;' method='post' action='/'>      
+                            <button style='background-color:#7700FF;' class='button primary'>History verlassen</button>   
+                        </form>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        "; 
+        //echo "<script>console.log('TurnierID: $TurnierID')</script>";
+        // button -> name='content'              
+    }else if ($test_turnier_id != 0) {
         //TurnierID überschreiben weil ich in den Test-Modus möchte
         $TurnierID = $testTurniere[$test_turnier_id][1]; //kommt aus variables.php
         $TurnierName = $testTurniere[$test_turnier_id][2];
