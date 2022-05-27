69abb179d87432f5184b478fe3bf61a749c348ab
richardbendler <richard.bendler@gmail.com>
2022-05-27 20:59:04 +0200

    team zertifikat

--- website_functionalities/generate_team_certificate/generate_team_certificate.php
+++ website_functionalities/generate_team_certificate/generate_team_certificate.php
@@ -58,14 +58,14 @@ $str = iconv('UTF-8', 'windows-1252', $str);
 utf8_decode();
 
 
-
+/*
 $pdf->Cell(0, 10, '' , 0, 1, 'C');
 $pdf->SetFont('Courier','',14);
 $pdf->Cell(0, 10, 'Vielen dank fuer deine Teilnahme am:' , 0, 1, 'C');
 $pdf->SetFont('Courier','B',18);
 $pdf->Cell(0, 10, 'BLANKIBALL-TURNIER 2021' , 0, 1, 'C');
 $pdf->Cell(0, 10, '' , 0, 1, 'C');
-/*
+
 include_once '../../database/db_connection.php';
 
 include_once '../../variables.php';
