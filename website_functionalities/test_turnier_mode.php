7c49b63ab770bf8d592833bedd3f792950cfd958
richardbendler <richard.bendler@gmail.com>
2022-05-28 16:49:57 +0200

    history

--- website_functionalities/test_turnier_mode.php
+++ website_functionalities/test_turnier_mode.php
@@ -20,6 +20,11 @@
                     <thead style='background-color:#7700FF;'>
                         <tr>
                             <td style='color:white; text-align: center;'>
+                                <!--<div style='position:absolute; top: 2px; right: 2px;'>-->
+                                    <form style='color:#00FF00;margin: 0 0 0 0;' method='post' action='/'>      
+                                        <button style='color:white; background-color:#7700FF;' class='button'>🔙 History verlassen</button>   
+                                    </form>
+                                <!--</div>-->
                                 <h3>History</p>
                                 <h2>$TurnierName</h2>
                                 <i> Du befindest dich in der History-Ansicht. 
@@ -37,11 +42,7 @@
                     </tbody>
                 </table>
             </div>
-            <div style='position:absolute; top: 2px; right: 2px;'>
-                <form style='color:#00FF00;margin: 0 0 0 0;' method='post' action='/'>      
-                    <button style='color:white; background-color:#7700FF;' class='button'>✖️ History verlassen</button>   
-                </form>
-            </div>
+            
         </div>
         <!-- <div style='background-color:#7700FF; text-align: center;'>
                 <h3>History</p>
