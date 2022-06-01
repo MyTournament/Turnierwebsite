00012e10f8c88f8d3213ed8405cf1fcb2e538bc4
richardbendler <richard.bendler@gmail.com>
2022-06-01 16:01:57 +0200

    try fixing backup

--- database/db_backup.php
+++ database/db_backup.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = 'REDACTED';
-$dbuser = 'REDACTED';
-$dbpassword = 'REDACTED';
-$dbname = 'REDACTED';
+$dbhost = "REDACTED";
+$dbuser = "REDACTED";
+$dbpassword = "REDACTED";
+$dbname = "REDACTED";
 /*
 //NORMALE DATEI
 $dumpfile = "db_backups/" . $dbname . "_" . date("Y-m-d_H-i-s") . ".sql";
@@ -20,6 +20,12 @@ $dumpfile = '/mnt/web508/d1/34/510124634/htdocs/Turnierwebsite/tourna/database/d
 //echo "Start dump\n";
 passthru("mysqldump --user=$dbuser --password=$dbpassword --host=$dbhost $dbname | gzip -c  > $dumpfile");
 //echo "-- Dump completed -- ";
+$sql = 'SELECT * FROM `System_Website`';
+$result = $conn->query($sql);
+while ($row = $result->fetch_assoc()) {
+    $name = $row['name'];
+    echo "-- Backup erstellt für website" + $name;
+}
 ////echo $dumpfile;
 
 
