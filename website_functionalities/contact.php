559124f93f28422814910756600678abdbec4e00
richardbendler <richard.bendler@gmail.com>
2022-12-23 13:16:05 +0100

    Client-seitiges Captcha - Serverseitig ist die Abfrage noch nicht fertig -> muss noch in contact.php und edit_teams.php implementiert werden

--- website_functionalities/contact.php
+++ website_functionalities/contact.php
@@ -8,14 +8,14 @@ include_once 'send_mail.php';
 $SECRET_KEY = "f3591a3b-4fdc-490c-99b0-a0b84ba5d938";    # replace with your secret key
 $VERIFY_URL = "https://hcaptcha.com/siteverify";
 # Retrieve token from post data with key 'h-captcha-response'.
-$token = request.POST_DATA['h-captcha-response']
+$token = request.POST_DATA['h-captcha-response'];
 # Build payload with secret key and token.
-$data = { 'secret': SECRET_KEY, 'response': token }
+$data = { 'secret': $SECRET_KEY, 'response': $token };
 # Make POST request with data payload to hCaptcha API endpoint.
-$response = http.post(url=VERIFY_URL, data=data)
+$response = http.post(url=$VERIFY_URL, data=$data);
 # Parse JSON from response. Check for success or error codes.
-$response_json = JSON.parse(response.content)
-$success = response_json['success']
+$response_json = JSON.parse(response.content);
+$success = response_json['success'];
 /*
 {
     "success": true|false,     // is the passcode valid, and does it meet security criteria you specified, e.g. sitekey?
