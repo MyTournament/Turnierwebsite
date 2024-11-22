ced9bb0650c87a5f4964d37ec2e5e8ccf8efe0cc
Bendler <richard.bendler@campana-schott.com>
2024-11-22 14:56:30 +0100

    automatic db backup in file system + also cleanup backups

--- database/db_backup.php
+++ database/db_backup.php
@@ -1,102 +1,156 @@
 <?php
-$dbhost = "REDACTED";
-$dbuser = "REDACTED";
-$dbpassword = "REDACTED";
-$dbname = "REDACTED";
+// Einbindung der db_connection.php für die Datenbankzugangsdaten
+include_once 'db_connection.php';
 
-//Gibt Pfad von Server aus:
-//echo getcwd();
+//WICHTIG: BERECHTIGUNGEN DER VERZEICHNISSE MÜSSEN STIMMEN (IN TRELLO DOKUMENTIERT)
 
 
-/*
-//NORMALE DATEI
-$dumpfile = "db_backups/" . $dbname . "_" . date("Y-m-d_H-i-s") . ".sql";
- 
-//echo "Start dump\n";
-exec("mysqldump --user=$dbuser --password=$dbpassword --host=$dbhost $dbname > $dumpfile");
-//echo "-- Dump completed -- ";
-//echo $dumpfile;
-*/
-
-//ZIP DATEI
-// TODO keinen absoluten Pfad benutzen, sondern dynamisch Pfad zu db_backups abrufen. Dadurch kann die Website in verschiedenen Umgebungen laufen. (https://stackoverflow.com/questions/7835948/include-once-relative-path-in-php)
-$dumpfile = '/mnt/web508/d1/34/510124634/htdocs/Turnierwebsite/tourna-dev/database/db_backups/' . $dbname . '_' . date("Y-m-d_H-i-s") . '.sql.gz';
-//Pfad nicht relativ sondern von backstage.php aus!
-
-//echo "Start dump\n";
-passthru("mysqldump --user=$dbuser --password=$dbpassword --host=$dbhost $dbname | gzip -c  > $dumpfile");
-passthru("mysqldump -u $dbuser -p $dbpassword -h $dbhost $dbname > gzip -c  > dump.sql");
-
-//echo "-- Dump completed -- ";
-/*$sql = 'SELECT * FROM `System_Website`';
-$result = $conn->query($sql);
-while ($row = $result->fetch_assoc()) {
-    $name = $row['name'];
-    echo "-- Backup erstellt für website" + $name;
-}*/
-////echo $dumpfile;
+// Hauptfunktionalität
+$backupDir = '/var/www/html/REDACTED/automatic_db_backups';
+// Backup erstellen und aufräumen
+try {
+    backupDatabase($conn, $backupDir);
+    cleanupBackups($backupDir);
+} catch (Exception $e) {
+    echo "<script>console.log('Fehler: Datenbankzugangsdaten konnten nicht abgerufen werden.')</script>";
+    die();
+}
 
 
-//PER MAIL VERSENDEN
-function mail_att($to, $from, $subject, $message, $file) {
-    // $to Empfänger
-    // $from Absender ("email@domain.de" oder "Name <email@domain.de>")
-    // $subject Betreff
-    // $message Inhalt der Email
-    // $file Pfad zur Datei die versendet werden soll
 
-    $mime_boundary = "-----=" . md5(uniqid(rand(), 1));
+// Backup-Funktion
+function backupDatabase($conn, $backupDir) {
+    //file_put_contents('backup_log_test.txt', 'Testinhalt');
 
-    $header = "From: ".$from."\r\n";
-    $header.= "MIME-Version: 1.0\r\n";
-    $header.= "Content-Type: multipart/mixed;\r\n";
-    $header.= " boundary=\"".$mime_boundary."\"\r\n";
+    // Holen der Zugangsdaten aus der Datenbankverbindung
+    //$db_host = $conn->host_info; // Host-Info enthält auch den Port
+    //$db_name = $conn->query("SELECT DATABASE()")->fetch_row()[0]; // Aktuelle Datenbank
+    //$db_user = $conn->user; // Benutzername
+    //$db_pass = $conn->passwd; // Passwort
+    $db_host = "REDACTED";
+    $db_user = "REDACTED";
+    $db_pass = "REDACTED";
+    $db_name = "REDACTED";
+    
+    // Prüfen, ob die Zugangsdaten korrekt gelesen wurden
+    if (!$db_host || !$db_name || !$db_user) {
+        die("Fehler: Datenbankzugangsdaten konnten nicht abgerufen werden.");
+    }
+    
+    // Name der Backup-Datei
+    //$backupFile = "../../automatic_db_backups/backup_" . $db_name . "_" . date("Ymd_His") . ".sql";
+    // Name der Backup-Datei mit vollständigem Pfad
+    $backupFile = $backupDir . "/backup_" . $db_name . "_" . date("Ymd_His") . ".sql";
 
-    $content = "This is a multi-part message in MIME format.\r\n\r\n";
-    $content.= "--".$mime_boundary."\r\n";
-    $content.= "Content-Type: text/plain charset=\"iso-8859-1\"\r\n";
-    $content.= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $content.= $message."\r\n";
+    // Befehl zum Erstellen des MySQL-Dumps
+    $command = sprintf(
+        'mysqldump --host=%s --user=%s --password=%s %s > %s',
+        escapeshellarg(explode(':', $db_host)[0]), // Nur der Host, ohne Port
+        escapeshellarg($db_user),
+        escapeshellarg($db_pass),
+        escapeshellarg($db_name),
+        escapeshellarg($backupFile)
+    );
 
-    //Datei anhaengen     
-    $name = basename($file);
-    $data = chunk_split(base64_encode(file_get_contents($file)));
-    $len = filesize($file);
-    $content.= "--".$mime_boundary."\r\n";
-    $content.= "Content-Disposition: attachment;\r\n";
-    $content.= "\tfilename=\"$name\";\r\n";
-    $content.= "Content-Length: .$len;\r\n";
-    $content.= "Content-Type: application/x-gzip; name=\"".$file."\"\r\n";
-    $content.= "Content-Transfer-Encoding: base64\r\n\r\n";
-    $content.= $data."\r\n"; 
+    // Debugging und Logging
+    $output = [];
+    $return_var = null;
 
-    return mail($to, $subject, $content, $header);
-}  
+    // Führen Sie den Befehl aus
+    exec($command . " 2>&1", $output, $return_var);
 
-mail_att("backup_collector@REDACTED.de", "backup_collector@REDACTED.de", "Backup ".$dumpfile, "Hier kommt mal wieder ein Update. Greetings Hermann", $dumpfile);
-//echo "<br>sent to backup_collector@REDACTED.de";
+    // Loggen Sie die Ausgabe in eine Datei
+    //file_put_contents('backup_log.txt', implode("\n", $output));
 
-//DATEIEN AUS BACKUP VERZEICHNIS WIEDER LÖSCHEN
-function deleteFilesFromDirectory($ordnername){
-    //überprüfen ob das Verzeichnis überhaupt existiert
-    if (is_dir($ordnername)) {
-        //Ordner öffnen zur weiteren Bearbeitung
-        if ($dh = opendir($ordnername)) {
-            //Schleife, bis alle Files im Verzeichnis ausgelesen wurden
-            while (($file = readdir($dh)) !== false) {
-                //Oft werden auch die Standardordner . und .. ausgelesen, diese sollen ignoriert werden
-                if ($file!="." AND $file !="..") {
-                    //Files vom Server entfernen
-                    unlink("".$ordnername."".$file."");
-                }
+    // Prüfen, ob das Backup erfolgreich war
+    if ($return_var === 0) {
+        //echo "<script>console.log('Backup erfolgreich: $backupFile')</script>";
+        echo "<script>console.log('Backup erfolgreich')</script>";
+    } else {
+        echo "<script>console.log('Backup fehlgeschlagen. Details siehe backup_log.txt')</script>";
+    }
+}
+
+
+
+
+
+
+function cleanupBackups($backupDir) {
+    $backups = glob($backupDir . "/backup_*.sql");
+    $now = time();
+
+    // Konfiguration für die Anzahl der Backups
+    $keep = [
+        'hourly' => 1000,       // Backups, die weniger als 1 Stunde alt sind
+        'daily' => 500,         // Backups, die weniger als 24 Stunden alt sind
+        'weekly' => 10,         // Backups der letzten 7 Tage
+        'forever' => 1          // Mindestens ein Backup pro Tag
+    ];
+
+    // Gruppierte Backups nach Alterskategorien
+    $groupedBackups = [
+        'hourly' => [],
+        'daily' => [],
+        'weekly' => [],
+        'forever' => []
+    ];
+
+    foreach ($backups as $file) {
+        preg_match('/backup_(.+?)_(\d{8}_\d{6})\.sql/', basename($file), $matches);
+        if (!$matches) {
+            continue; // Datei hat nicht das erwartete Namensschema
+        }
+
+        $timestamp = DateTime::createFromFormat("Ymd_His", $matches[2])->getTimestamp();
+        $age = $now - $timestamp;
+        $date = date("Y-m-d", $timestamp);
+
+        if ($age < 3600) { // Weniger als 1 Stunde
+            $groupedBackups['hourly'][] = $file;
+        } elseif ($age < 86400) { // Weniger als 24 Stunden
+            $groupedBackups['daily'][] = $file;
+        } elseif ($age < 604800) { // Weniger als 7 Tage
+            $groupedBackups['weekly'][$date][] = $file;
+        } else { // Älter als 7 Tage
+            if (!isset($groupedBackups['forever'][$date])) {
+                $groupedBackups['forever'][$date] = $file; // Nur ein Backup pro Tag
             }
-            //geöffnetes Verzeichnis wieder schließen
-            closedir($dh);
+        }
+    }
+
+    // Backups bereinigen nach Kategorien
+    foreach ($groupedBackups as $key => $files) {
+        if ($key === 'weekly' || $key === 'forever') {
+            continue; // Diese Kategorien werden separat behandelt
+        }
+
+        if (count($files) > $keep[$key]) {
+            $toDelete = array_slice($files, 0, count($files) - $keep[$key]);
+            foreach ($toDelete as $file) {
+                unlink($file);
+            }
+        }
+    }
+
+    // Wochenbackups reduzieren
+    foreach ($groupedBackups['weekly'] as $date => $files) {
+        if (count($files) > $keep['weekly']) {
+            $toDelete = array_slice($files, 0, count($files) - $keep['weekly']);
+            foreach ($toDelete as $file) {
+                unlink($file);
+            }
+        }
+    }
+
+    // Ältere Backups bereinigen (nur ein Backup pro Tag behalten)
+    foreach ($groupedBackups['forever'] as $date => $file) {
+        if (!in_array($file, $groupedBackups['hourly']) && !in_array($file, $groupedBackups['daily']) && !isset($groupedBackups['weekly'][$date])) {
+            // Ein Backup behalten, alle anderen löschen
+            $toDelete = array_slice($groupedBackups['forever'], 1);
+            foreach ($toDelete as $file) {
+                unlink($file);
+            }
         }
     }
 }
-//Funktionsaufruf - Directory immer mit endendem / angeben
-// TODO keinen absoluten Pfad benutzen, sondern dynamisch Pfad zu db_backups abrufen. Dadurch kann die Website in verschiedenen Umgebungen laufen. (https://stackoverflow.com/questions/7835948/include-once-relative-path-in-php)
-//deleteFilesFromDirectory("/mnt/web508/d1/34/510124634/htdocs/REDACTED/website/database/db_backups/");
-
-?>
